2932a10580497f89b8929aa83923eb4d09c8129d
Shvms El Deen <110482765+shvmseldeen@users.noreply.github.com>
2026-03-18 09:24:13 +0200

    Update and rename index.tsx to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { serveStatic } from move;
+import { serveStatic } from "./static";
 import { createServer } from "http";
 
 const app = express();
